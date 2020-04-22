72a87d8d1bd1cbb2d12d1095f3f8778408f16d04
jeonhs9519 <jeonhs9519@gmail.com>
2020-04-22 12:26:33 +0900

    Updates

---
+++ precache-manifest.4d60441cfb51560d5796bf1231bb93aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe6329756553978e9008b5eebbf4f5b",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "32ed930c6cd7015872be",
+    "url": "/movie_app_2019/static/css/main.34886264.chunk.css"
+  },
+  {
+    "revision": "31d23c861c671296d1d9",
+    "url": "/movie_app_2019/static/js/2.81592806.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.81592806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ed930c6cd7015872be",
+    "url": "/movie_app_2019/static/js/main.909a78bb.chunk.js"
+  },
+  {
+    "revision": "cae58084a17fa671244e",
+    "url": "/movie_app_2019/static/js/runtime-main.4e485941.js"
+  }
+]);
